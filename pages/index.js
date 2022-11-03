5866229ee5b7c8a6e687077cd052fc6e206705f0
MinlabHQ <iskandarm20d@student.unhas.ac.id>
2022-11-03 18:45:41 +0800

    Add card contents

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
+import CardContent from "../components/CardContent";
 import Link from "next/link";
 import React from "react";
-import styles from "../styles/Home.module.css";
+import Head from "next/head";
 
 export default function Index() {
     return (
-        <div className={styles.main}>
-            <h1 className={styles.title}>Home</h1>
-            <Link href="/account">
-                Login
-            </Link>
-        </div>
+        <>
+            <Head>
+                <title>Home</title>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700&family=Raleway:wght@300;400;500;700&family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+            </Head>
+            <div>
+                <h1>Home</h1>
+                <Link href="/account">
+                    Login
+                </Link>
+                <div className="grid grid-cols-3 w-screen my-8">
+                    <CardContent />
+                    <CardContent />
+                    <CardContent />
+                    <CardContent />
+                    <CardContent />
+                    <CardContent />
+                </div>
+            </div>
+        </>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,59 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    fontFamily: {
+      sans: ["Space Grotesk", "Raleway", "Montserrat", "sans-serif"],
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        'black': {
+          'primary': '#ffffff',
+          'primary-focus': '#ffffff',
+          'primary-content': '#000000',
+
+          'secondary': '#ffffff',
+          'secondary-focus': '#ffffff',
+          'secondary-content': '#000000',
+
+          'accent': '#ffffff',
+          'accent-focus': '#ffffff',
+          'accent-content': '#000000',
+
+          'neutral': '#333333',
+          'neutral-focus': '#4d4d4d',
+          'neutral-content': '#ffffff',
+
+          'base-100': '#ffffff',
+          'base-200': '#333333',
+          'base-300': '#4d4d4d',
+          'base-content': '#000000',
+
+          'info': '#0000ff',
+          'success': '#008000',
+          'warning': '#ffff00',
+          'error': '#ff0000',
+
+          '--rounded-box': '0',
+          '--rounded-btn': '0',
+          '--rounded-badge': '0',
+
+          '--animation-btn': '0',
+          '--animation-input': '0',
+
+          '--btn-text-case': 'lowercase',
+          '--navbar-padding': '0.5rem',
+          '--border-btn': '1px',
+        },
+      },
+    ],
+  },
 }
