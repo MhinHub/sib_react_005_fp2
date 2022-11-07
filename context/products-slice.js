07814bd92f363b7666554388ce595ceeb167f56c
MinlabHQ <iskandarm20d@student.unhas.ac.id>
2022-11-07 19:05:32 +0800

    fix some state data store

--- context/products-slice.js
+++ context/products-slice.js
@@ -5,83 +5,79 @@ const initialState = {
   cart: [],
   checkout: [],
   firstRender: true,
-  stock: [10],
 };
 
 export const ProductsSlice = createSlice({
-    name: 'Products',
-    initialState,
-    reducers: {
-        initiateProducts: (state, action) => {
-            state.firstRender = false;
-            state.products = action.payload;
-        },
-        updateStock: (state, action) => {
-            state.products[action.payload.idx].stock = action.payload.stock;
-        },
-        addCart: (state, action) => {
-            let idx = -1;
+  name: "Data Product Slice",
+  initialState,
+  reducers: {
+    initiateProducts: (state, action) => {
+      state.firstRender = false;
+      state.products = action.payload;
+    },
+    updateStock: (state, action) => {
+      state.products[action.payload.idx].stock = action.payload.stock;
+    },
+    addCart: (state, action) => {
+      let idx = -1;
 
-            const { cartData, qty, isCart } = action.payload;
+      const { cartData, qty, isCart } = action.payload;
 
-            const isProductFound = state.cart.some((data, dataIdx) => {
-                if (data.id === cartData.id) {
-                    idx = dataIdx;
-                    return true;
-                }
-                return false;
-            });
-
-            if (isProductFound) {
-                if (isCart)
-                    qty
-                      ? (state.cart[idx].cartQuantity = parseInt(qty))
-                      : (state.cart[idx].cartQuantity = qty); 
-                else
-                    state.cart[idx].cartQuantity += parseInt(qty); 
-                state.cart[idx].isOverStock =
-                  state.cart[idx].stock < state.cart[idx].cartQuantity;
-            } else {
-                state.cart.push({
-                  ...cartData,
-                  cartQuantity: qty,
-                  isOverStock: cartData.stock < 1,
-                });
-            }
-        },
-        addCheckout: (state) => {
-            let idx = -1;
-            state.cart.forEach(item => {
-                if (item.cartQuantity >= 1) {
-                    const isProductFound = state.checkout.some((data, dataIdx) => {
-                        if (data.id === item.id) {
-                            idx = dataIdx;
-                            return true;
-                        }
-                        return false;
-                    });
-
-                    if (isProductFound)
-                        state.checkout[idx].sold += parseInt(item.cartQuantity);
-                    else
-                        state.checkout.push({ ...item, sold: item.cartQuantity });
-
-                    state.products.forEach((product, productIdx) => {
-                        if (product.id === item.id)
-                            state.products[productIdx].stock -=
-                              item.cartQuantity;
-                    })
-                }
-            })
-
-            state.cart = [];
-        },
-        deleteCart: (state, action) => {
-            state.cart.splice(
-              state.cart.findIndex((data) => data.id === action.payload)
-            );
+      const isProductFound = state.cart.some((data, dataIdx) => {
+        if (data.id === cartData.id) {
+          idx = dataIdx;
+          return true;
         }
-    }
+        return false;
+      });
+
+      if (isProductFound) {
+        if (isCart)
+          qty
+            ? (state.cart[idx].cartQuantity = parseInt(qty))
+            : (state.cart[idx].cartQuantity = qty);
+        else state.cart[idx].cartQuantity += parseInt(qty);
+        state.cart[idx].isOverStock =
+          state.cart[idx].stock < state.cart[idx].cartQuantity;
+      } else {
+        state.cart.push({
+          ...cartData,
+          cartQuantity: qty,
+          isOverStock: cartData.stock < 1,
+        });
+      }
+    },
+    addCheckout: (state) => {
+      let idx = -1;
+      state.cart.forEach((item) => {
+        if (item.cartQuantity >= 1) {
+          const isProductFound = state.checkout.some((data, dataIdx) => {
+            if (data.id === item.id) {
+              idx = dataIdx;
+              return true;
+            }
+            return false;
+          });
+
+          if (isProductFound)
+            state.checkout[idx].sold += parseInt(item.cartQuantity);
+          else state.checkout.push({ ...item, sold: item.cartQuantity });
+
+          state.products.forEach((product, productIdx) => {
+            if (product.id === item.id)
+              state.products[productIdx].stock -= item.cartQuantity;
+          });
+        }
+      });
+
+      state.cart = [];
+    },
+    deleteCart: (state, action) => {
+      state.cart.splice(
+        state.cart.findIndex((data) => data.id === action.payload)
+      );
+    },
+  },
 });
 
 export const {
