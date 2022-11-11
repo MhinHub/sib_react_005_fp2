ea9b80bb0665ae7a22f6715191e86159bde67236
MinlabHQ <iskandarm20d@student.unhas.ac.id>
2022-11-11 14:17:19 +0800

    update responsively component

--- components/Footer.js
+++ components/Footer.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer>
-            <div className="flex justify-center items-center bg-black text-white mt-40 py-4 bottom-0 relative">
-                <p>© 2022 Hashop. All rights reserved.</p>
-            </div>
-        </footer>
-    )
+      <footer>
+        <div className="flex justify-center items-center bg-black text-white mt-40 py-4 mb-0 align-bottom bottom-0 relative">
+          <p>© 2022 Hashop. All rights reserved.</p>
+        </div>
+      </footer>
+    );
 }
