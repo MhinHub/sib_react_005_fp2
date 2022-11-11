9fdf7f5ec524de72ddedd9ce0079bc7b104db49b
MinlabHQ <iskandarm20d@student.unhas.ac.id>
2022-11-11 16:16:47 +0800

    update meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import Head from "next/head";
 import Nav from "./Nav";
 import CartUser from "./modal/CartUser";
-import Footer from "./Footer"
+import Footer from "./Footer";
 import ProductDetail from "./modal/ProductDetail_";
 import Login from "./modal/Login";
 
@@ -13,10 +13,6 @@ export default function layout({ title, children }) {
         <meta charSet="UTF-8" />
         <title>{`${title} | Hashop`}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Hashop is best of the best best online shopping website"
-        />
       </Head>
       <main>
         <Login />
--- pages/_document.js
+++ pages/_document.js
@@ -36,6 +36,61 @@ export default class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon-192x192.png" />
           <meta name="theme-color" content="#bca6ff" />
+          <meta name="msapplication-navbutton-color" content="#bca6ff" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="#bca6ff"
+          />
+          <meta
+            name="description"
+            content="Hashop is best of the best best online shopping website app with the best fashion products for unisex also with accessories and electronics as well."
+          />
+          <meta
+            name="keywords"
+            content="hashop, hashop ecommerce, hashop shop, ecommerce, online shopping, online shop, online store, online ecommerce, online ecommerce shop, online ecommerce store, online ecommerce website, online ecommerce app, online ecommerce application, online ecommerce platform, online ecommerce software, online ecommerce solution, minimalist online ecommerce design system, online ecommerce website, online ecommerce website, online ecommerce website design, online ecommerce website development, online ecommerce website template, online ecommerce website app, online ecommerce website app free, online ecommerce website app, online ecommerce website app react with css, online ecommerce website app react with next js online ecommerce website app react with next js, online ecommerce website app react with next js redux toolkit, minimalist online ecommerce website app react with next js redux toolkit tailwind, online ecommerce website app react with next js redux toolkit tailwind responsive, online ecommerce website app react with next js redux toolkit tailwind responsive free, online ecommerce website app react with next js redux toolkit tailwind responsive, online ecommerce website app react with next js redux toolkit tailwind responsive 2022, online ecommerce website app react with next js redux toolkit tailwind responsive 2022, online ecommerce website"
+          />
+          <meta
+            name="author"
+            content="Muhaemin Iskandar, Hacktiv8 - Kampus Merdeka"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Hashop - Ecommerce app" />
+          <meta
+            property="og:description"
+            content="Hashop is best of the best best online shopping website app with the best fashion products for unisex also with accessories and electronics as well."
+          />
+          <meta property="og:image" content="/icon-192x192.png" />
+          <meta property="og:url" content="https://hashop-store.vercel.app/" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Hashop - Ecommerce app" />
+          <meta
+            name="twitter:description"
+            content="Hashop is best of the best best online shopping website app with the best fashion products for unisex also with accessories and electronics as well."
+          />
+          <meta name="twitter:image" content="/icon-192x192.png" />
+          <meta name="twitter:image:alt" content="Hashop" />
+          <meta
+            name="twitter:domain"
+            content="https://hashop-store.vercel.app/"
+          />
+          <meta
+            name="twitter:app:url:iphone"
+            content="https://hashop-store.vercel.app/"
+          />
+          <meta
+            name="twitter:app:url:ipad"
+            content="https://hashop-store.vercel.app/"
+          />
+
+          <meta name="twitter:app:country" content="US" />
+          <meta name="twitter:app:country" content="ID" />
+
+          <meta name="robots" content="index, follow" />
+          <meta name="googlebot" content="index, follow" />
+          <meta
+            name="google-site-verification"
+            content="7KgkV67H12Dm3cvEblpXUAYgoT9BpUzCN8Cw-JXVQjc"
+          />
         </Head>
         <body>
           <Main />
