4fcad418fbd0e8ca8ff551131eafbe0d13c3adfc
MinlabHQ <iskandarm20d@student.unhas.ac.id>
2022-11-12 15:58:50 +0800

    add starter about page

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function about() {
+  return (
+    <>
+      <h1 className="text-start text-5xl font-bold my-10">About</h1>
+      <div className="flex justify-center">
+        <iframe
+          src="https://trello.com/b/B6jRcl3z.html"
+          width="80%"
+          height="600"
+        />
+      </div>
+    </>
+  );
+}
