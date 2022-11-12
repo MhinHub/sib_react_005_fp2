35623d340d381a8cdafd74d27c534e4dbf7c0d5d
MinlabHQ <iskandarm20d@student.unhas.ac.id>
2022-11-12 13:54:53 +0800

    add style responsively hero product component

--- components/HeroProduct.js
+++ components/HeroProduct.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 function TxtSide({ title, desc, txtBtn }) {
   return (
-    <div className="flex flex-col w-2/5 justify-center">
+    <div className="flex flex-col lg:w-2/5 justify-center">
       <h2 className="text-4xl font-bold">{title || "Title"}</h2>
       <p className="text-base font-mono my-3">{desc || "Description"}</p>
       <button className="bg-black text-white px-4 py-2 rounded-none mt-4">
@@ -15,7 +15,7 @@ function TxtSide({ title, desc, txtBtn }) {
 
 function ImgSide({ src }) {
   return (
-    <div className="flex w-1/2 justify-center">
+    <div className="flex w-1/2 self-center justify-center">
       <Image
         src={src}
         alt="product hero"
@@ -30,7 +30,7 @@ function ImgSide({ src }) {
 
 export default function HeroProduct({ side, title, desc, txtBtn, src }) {
   return (
-    <div className="flex justify-around border-t border-solid border-black px-10 py-12 mt-16 bg-white">
+    <div className="flex lg:flex-row flex-col justify-around border-t border-solid border-black px-10 py-12 mt-16 bg-white">
       {side === true ? (
         <>
           <TxtSide title={title} desc={desc} txtBtn={txtBtn} />
@@ -38,8 +38,8 @@ export default function HeroProduct({ side, title, desc, txtBtn, src }) {
         </>
       ) : (
         <>
-            <ImgSide src={src} />
-            <TxtSide title={title} desc={desc} txtBtn={txtBtn} />
+          <ImgSide src={src} />
+          <TxtSide title={title} desc={desc} txtBtn={txtBtn} />
         </>
       )}
     </div>
