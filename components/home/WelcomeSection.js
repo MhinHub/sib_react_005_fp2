eb14613cdf1bf5e9783abaadcad4f71f38d99956
MhinHub <iskandarm20d@student.unhas.ac.id>
2023-11-27 20:23:55 +0800

    update: add button to hide top marquee in welcome section

--- components/home/WelcomeSection.js
+++ components/home/WelcomeSection.js
@@ -1,8 +1,12 @@
 import Marquee from "react-fast-marquee";
 import { useLottie } from "lottie-react";
+import { useState } from "react";
+import { cn } from "../../lib/utils";
 import DownArrow from "../../public/lottie/down-black-arrow.json";
 
 export default function Welcome() {
+  const [hiddenMarquee, setHiddenMarquee] = useState(false);
+
   const options = {
     animationData: DownArrow,
     loop: true,
@@ -14,7 +18,10 @@ export default function Welcome() {
   return (
     <section className="flex flex-col h-screen w-full bg-image-linear-gradient-v">
       {/* create marquee rectangle in bottom screen */}
-      <div className="flex absolute bg-black h-14 !w-screen items-center justify-center animate-marquee">
+      <div className={cn("flex absolute bg-black h-14 !w-screen items-center justify-center animate-marquee",
+        hiddenMarquee && "hidden",
+        "transition-all duration-1000 ease-in-out"
+      )}>
         <Marquee
           pauseOnHover
           gradient={false}
@@ -33,6 +40,12 @@ export default function Welcome() {
           <p className="px-5">Using Fakestoreapi</p>
           <p className="px-2">🧑‍💻</p>
         </Marquee>
+        <button
+          className="absolute right-0 mr-5 bg-white rounded-full w-8 h-8 flex justify-center items-center z-10"
+          onClick={() => setHiddenMarquee(true)}
+        >
+          x
+        </button>
       </div>
       <div className="flex flex-col my-auto items-center">
         <h1 className="lg:text-9xl md:text-7xl text-5xl text-center font-extrabold">
